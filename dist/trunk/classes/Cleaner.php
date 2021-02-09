fa66b762ccf6292385caa2226647e0b6ec0281b1
jhotadhari <tellme@waterproof-webdesign.info>
2021-02-09 17:19:31 +0100

    ## 0.2.0 - 2021-02-09
    Change hook priorities to enable woocommerce-blocks to add inline script data wcSettings
    
    ### Added
    - Filters `acll_loader_hook_priorities`|`acll_cleaner_hook_priorities` to allow hook priorities to be filtered
    
    ### Changed
    - Hook priorities. To enable woocommerce-blocks to add inline script data wcSettings
    - Only load assets when they are not already loaded on start

--- dist/trunk/classes/Cleaner.php
+++ dist/trunk/classes/Cleaner.php
@@ -33,16 +33,40 @@ class Cleaner {
 	}
 
 	protected function __construct() {
-        // Dequeue scripts. Twice, in header and in footer.
-		add_action( 'wp_print_scripts', array( $this, 'dequeue_scripts' ), 999 );
-        add_action( 'wp_print_footer_scripts', array( $this, 'dequeue_scripts' ), 3 );
-        add_filter( 'print_scripts_array', array( $this, 'remove_from_print_scripts_array' ), 10, 1 );
-        // Dequeue styles
-        add_action( 'wp_print_styles', array( $this, 'dequeue_styles' ), 999 );
-        add_filter( 'print_styles_array', array( $this, 'remove_from_print_styles_array' ), 10, 1 );
-        // Remove some dependencies from all woo block scripts
-		add_filter( 'woocommerce_blocks_register_script_dependencies', array( $this, 'woo_blocks_dependencies' ), 999, 1 );
+        add_action( 'wp_loaded', array( $this, 'hooks' ), 20 );
 	}
+
+    /**
+     * Setup hooks.
+     */
+	public function hooks() {
+        // Allow priorities to be filtered.
+        $priorities = apply_filters( 'acll_cleaner_hook_priorities', array(
+            'dequeue_scripts_header'            => 999,
+            'dequeue_scripts_footer'            => 3,
+            'remove_from_print_scripts_array'   => 10,
+            'dequeue_styles'                    => 999,
+            'remove_from_print_styles_array'    => 10,
+            'woo_blocks_dependencies'           => 999,
+        ) );
+
+        // Dequeue scripts. Twice, in header and in footer.
+		add_action( 'wp_print_scripts', array( $this, 'dequeue_scripts_header' ), Arr::get( $priorities, 'dequeue_scripts_header' ) );
+        // Dequeue footer scripts.
+        // Has do be done after woocommerceBlocks added the inline script data wcSettings. See Automattic\WooCommerce\Blocks\Assets\AssetDataRegistry::enqueue_asset_data in woo-gutenberg-products-block/src/Assets/AssetDataRegistry.php
+        // And before Loader collected scripts in footer. See Loader::hooks
+        add_action( 'wp_print_footer_scripts', array( $this, 'dequeue_scripts_footer' ), Arr::get( $priorities, 'dequeue_scripts_footer' ) );
+        // If scripts somehow remain in the queue, remove them just before they are going to be printed.
+        add_filter( 'print_scripts_array', array( $this, 'remove_from_print_scripts_array' ), Arr::get( $priorities, 'remove_from_print_scripts_array' ), 1 );
+
+        // Dequeue styles
+        add_action( 'wp_print_styles', array( $this, 'dequeue_styles' ), Arr::get( $priorities, 'dequeue_styles' ) );
+        // If styles somehow remain in the queue, remove them just before they are going to be printed.
+        add_filter( 'print_styles_array', array( $this, 'remove_from_print_styles_array' ), Arr::get( $priorities, 'remove_from_print_styles_array' ), 1 );
+
+        // Remove some dependencies from all woo block scripts
+		add_filter( 'woocommerce_blocks_register_script_dependencies', array( $this, 'woo_blocks_dependencies' ), Arr::get( $priorities, 'woo_blocks_dependencies' ), 1 );
+    }
 
     /**
      * Get an array of script handles.
@@ -75,6 +99,12 @@ class Cleaner {
             wp_dequeue_script( $handle );
         }
     }
+    public function dequeue_scripts_header() {
+        $this->dequeue_scripts();
+    }
+    public function dequeue_scripts_footer() {
+        $this->dequeue_scripts();
+    }
 
     /**
      * Dequeue certain styles
@@ -87,6 +117,7 @@ class Cleaner {
 
     /**
      * Remove scripts from the array of enqueued styles before processing for output.
+     * Note: this removes just the script itself, not its dependencies.
      */
     public function remove_from_print_scripts_array( $handles ) {
         $dequeue_handles = $this->get_dequeue_handles( 'script' );
@@ -97,6 +128,7 @@ class Cleaner {
 
     /**
      * Remove styles from the array of enqueued styles before processing for output.
+     * Note: this removes just the style itself, not its dependencies.
      */
     public function remove_from_print_styles_array( $handles ) {
         $dequeue_handles = $this->get_dequeue_handles( 'style' );
